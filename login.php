18e51082b2c87f51fd192883c994f1d6d0ab2d9f
VictorAugusto101 <victor102033156261@gmail.com>
2026-09-01 21:27:22 -0300

    3

--- login.php
+++ login.php
@@ -1,23 +1,40 @@
 <?php
+
+include("conexao.php");
+
 $cpf = $_POST['cpf'];
 $senha = $_POST['senha'];
 
 if ($cpf == ''){
-    echo 'CPF vazio, digite o CPF';
+    die('digite o cpf');
 }
 
 if ($senha == ''){
-    echo 'senha vazia, digite a senha';
+    die('digite a senha');
 }
 
-if($cpf == '123' && $senha == '123'){
-    session_start();
-    $_SESSION['cpf'] = $cpf;
-    $_SESSION['senha'] = $senha;
-    $_SESSION['nome'] = 'Victor';
-    header('Location: principal.php');
+$sql = "select nome from usuarios where cpf = ? and senha = ?";
+$stmt = $conn->prepare($sql);
 
+if($stmt){
+    $stmt->bind_param("ss",$cpf,$senha);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result->num_rows >0){
+        $row = $result->fetch_assoc();
+        if($row['nome'] != ''){
+            session_start();
+            $_SESSION['cpf'] = $cpf;
+            $_SESSION['senha'] = $senha;
+            $_SESSION['nome'] = 'Victor';
+            header('Location: principal.php');
+        }else {
+            echo 'usuario ou senha incorretos';
+        }
+    }else{
+        echo 'usuario ou senha incorretos';
+    }
 }else{
-    echo 'senha errrada';
+    echo 'falha no sql';
 }
 ?>
